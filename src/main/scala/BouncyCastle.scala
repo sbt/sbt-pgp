cb143d154138650744d9d5fbd2d44d2971dfe56f
Josh Suereth <joshua.suereth@gmail.com>
2011-10-02 13:48:33 -0400

    Fixed key creation so it uses random keys.  This is *way* better than the hardcoded version before, but can be slow.

--- src/main/scala/BouncyCastle.scala
+++ src/main/scala/BouncyCastle.scala
@@ -241,10 +241,10 @@ object BouncyCastle {
     val elgKeyPair = {
       val generator = KeyPairGenerator.getInstance("ELGAMAL", "BC")
       // TODO - Maybe we should pick these randomnly i.e. not pregenerate the parameters.
-      val g = new BigInteger("153d5d6172adb43045b68ae8e1de1070b6137005686d29d3d73a7749199681ee5b212c9b96bfdcfa5b20cd5e3fd2044895d609cf9b410b7a0f12ca1cb9a428cc", 16)
-      val p = new BigInteger("9494fec095f3b85ee286542b3836fc81a5dd0a0349b4c239dd38744d488cf8e31db8bcb7d33b41abb9e5a33cca9144b1cef332c94bf0573bf047a3aca98cdf3b", 16)
-      val elParams = new ElGamalParameterSpec(p, g)
-      generator.initialize(elParams)
+      //val g = new BigInteger("153d5d6172adb43045b68ae8e1de1070b6137005686d29d3d73a7749199681ee5b212c9b96bfdcfa5b20cd5e3fd2044895d609cf9b410b7a0f12ca1cb9a428cc", 16)
+      //val p = new BigInteger("9494fec095f3b85ee286542b3836fc81a5dd0a0349b4c239dd38744d488cf8e31db8bcb7d33b41abb9e5a33cca9144b1cef332c94bf0573bf047a3aca98cdf3b", 16)
+      //val elParams = new ElGamalParameterSpec(p, g)
+      //generator.initialize(elParams)
       generator.generateKeyPair()
     }
    
