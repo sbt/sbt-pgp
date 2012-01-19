a33a70c247e3c9d3a642c59f8b16bd8381bc6dc9
Josh Suereth <joshua.suereth@gmail.com>
2012-01-19 08:51:53 -0500

    Source dependencies on your previous version = bad.  Will have to keep PGP config for deploying the PGP plugin local.

--- project/project/Build.scala
+++ project/project/Build.scala
@@ -1,7 +1,7 @@
 import sbt._
 object PluginDef extends Build {
   override def projects = Seq(root)
-  lazy val root = Project("plugins", file(".")) dependsOn(ghpages, pgp)
+  lazy val root = Project("plugins", file(".")) dependsOn(ghpages)
   lazy val ghpages = uri("git://github.com/jsuereth/xsbt-ghpages-plugin.git")
-  lazy val pgp = uri("git://github.com/sbt/xsbt-gpg-plugin.git#0.3")
+  //lazy val pgp = uri("git://github.com/sbt/xsbt-gpg-plugin.git#0.3")
 }
