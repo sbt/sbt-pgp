0f624a9f1f5026ac3bf32b90f1ff2926a109e07b
jozic <jozic@ua.fm>
2012-07-30 09:09:44 -0400

    if passphrase is incorrect ask 3 times or giveup immediately

--- src/main/scala/SbtPgpCommandContext.scala
+++ src/main/scala/SbtPgpCommandContext.scala
@@ -6,9 +6,7 @@ import sbt.Keys.TaskStreams
 
 case class SbtPgpStaticContext(
     publicKeyRingFile: File,
-    secretKeyRingFile: File) extends cli.PgpStaticContext {
-  
-}
+    secretKeyRingFile: File) extends cli.PgpStaticContext
 
 /** Context used by PGP commands as they execute. */
 case class SbtPgpCommandContext(
@@ -23,12 +21,33 @@ case class SbtPgpCommandContext(
   def readHidden(msg: String): String = System.out.synchronized {
     SimpleReader.readLine(msg, Some('*')) getOrElse sys.error("Failed to grab input")
   }
-  def inputPassphrase = readHidden("Please enter PGP passphrase: ").toCharArray
-  def withPassphrase[U](f: Array[Char] => U): U =
-    PasswordCache.withValue(
+  def inputPassphrase = readHidden("Please enter PGP passphrase (or ENTER to abort): ") match {
+    case s: String if (!s.isEmpty) => s.toCharArray
+    case _ => sys.error("Empty passphrase. aborting...")
+  }
+
+  def withPassphrase[U](f: Array[Char] => U): U = {
+    retry[U, IncorrectPassphraseException](3){
+      PasswordCache.withValue(
         key = ctx.secretKeyRingFile.getAbsolutePath,
         default = optPassphrase getOrElse inputPassphrase)(f)
-  
+    } match {
+      case Right(u) => u
+      case Left(e) => sys.error("Wrong passphrase. aborting...")
+    }
+  }
+
+  @annotation.tailrec
+  private def retry[A, E <: Exception](n: Int)(body: => A)(implicit desired: Manifest[E]): Either[E, A] = {
+    try
+      return Right(body)
+    catch {
+      case e: Exception if desired.erasure.isAssignableFrom(e.getClass) => if (n <= 1) return Left(e.asInstanceOf[E])
+    }
+    retry[A, E](n - 1)(body)
+  }
+
+
   def log = s.log
   // TODO - Is this the right thing to do?
   def output[A](msg: => A): Unit = println(msg)
