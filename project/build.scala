74edf9d650099b012d56fd0fd6b4d45a6853c3e6
Josh Suereth <joshua.suereth@gmail.com>
2013-02-12 11:36:06 -0500

    Bumped version for 0.8 release.

--- project/build.scala
+++ project/build.scala
@@ -62,7 +62,7 @@ object Sonatype {
 object GpgBuild extends Build {
   val defaultSettings: Seq[Setting[_]] = Seq(
     organization := "com.jsuereth",
-    version := "0.8-SNAPSHOT",
+    version := "0.8",
     publishMavenStyle := false,
     publishTo <<= (version) { version: String =>
        val scalasbt = "http://repo.scala-sbt.org/scalasbt/"
